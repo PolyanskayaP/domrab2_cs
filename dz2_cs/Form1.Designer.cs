f9dca8d971c3b266527904267a6faddc62ca8733
Полина Полянская <ppolina353@yandex.ru>
2022-10-27 19:21:18 +0300

    Добавьте файлы проекта.

---
+++ dz2_cs/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace dz2_cs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.vvod = new System.Windows.Forms.TextBox();
+            this.tb_ot = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.vivod = new System.Windows.Forms.TextBox();
+            this.tb_do = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // vvod
+            // 
+            this.vvod.Location = new System.Drawing.Point(12, 36);
+            this.vvod.Name = "vvod";
+            this.vvod.Size = new System.Drawing.Size(552, 22);
+            this.vvod.TabIndex = 0;
+            this.vvod.TextChanged += new System.EventHandler(this.vvod_TextChanged);
+            // 
+            // tb_ot
+            // 
+            this.tb_ot.Location = new System.Drawing.Point(12, 129);
+            this.tb_ot.Name = "tb_ot";
+            this.tb_ot.Size = new System.Drawing.Size(100, 22);
+            this.tb_ot.TabIndex = 1;
+            this.tb_ot.TextChanged += new System.EventHandler(this.tb_ot_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(238, 286);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 94);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Изменить";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // vivod
+            // 
+            this.vivod.Location = new System.Drawing.Point(12, 231);
+            this.vivod.Name = "vivod";
+            this.vivod.ReadOnly = true;
+            this.vivod.Size = new System.Drawing.Size(552, 22);
+            this.vivod.TabIndex = 5;
+            this.vivod.TextChanged += new System.EventHandler(this.vivod_TextChanged);
+            // 
+            // tb_do
+            // 
+            this.tb_do.Location = new System.Drawing.Point(208, 129);
+            this.tb_do.Name = "tb_do";
+            this.tb_do.Size = new System.Drawing.Size(100, 22);
+            this.tb_do.TabIndex = 6;
+            this.tb_do.TextChanged += new System.EventHandler(this.tb_do_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Исходная строка";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 212);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(137, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Изменённая строка";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 16);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "1ая граница";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(222, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 16);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "2ая граница";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(583, 402);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tb_do);
+            this.Controls.Add(this.vivod);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tb_ot);
+            this.Controls.Add(this.vvod);
+            this.Name = "Form1";
+            this.Text = "dz2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox vvod;
+        private System.Windows.Forms.TextBox tb_ot;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox vivod;
+        private System.Windows.Forms.TextBox tb_do;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ dz2_cs/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dz2_cs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void vvod_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tb_ot_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tb_do_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void vivod_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void algor(String data)
+        {
+            String str_ot = tb_ot.Text;
+            String str_do = tb_do.Text;
+            if (str_ot == "")
+            {
+                vivod.Text = "";
+                MessageBox.Show(
+                    "Вы ничего не ввели в поле с 1ой границей."
+                    );
+            }
+            else
+            if (str_ot.All(Char.IsDigit) == false && str_ot[0] != '-')
+            {
+                vivod.Text = "";
+                MessageBox.Show(
+                    "В поле с 1ой границей должны быть цифры."
+                    );
+            }
+            else 
+            if (str_ot.All(Char.IsDigit) || ((str_ot.Substring(1)).All(Char.IsDigit)) && str_ot[0] == '-') 
+            {
+                if (str_do == "")
+                {
+                    vivod.Text = "";
+                    MessageBox.Show(
+                        "Вы ничего не ввели в поле со 2ой границей."
+                        );
+                }
+                else
+                if (str_do.All(Char.IsDigit) == false && str_do[0] != '-')
+                {
+                    vivod.Text = "";
+                    MessageBox.Show(
+                        "В поле со 2ой границей должны быть цифры."
+                        );
+                }
+                else
+                {
+                    String prov_ot = str_ot.Substring(1);
+                    String prov_do = str_do.Substring(1);
+
+                    if (str_do.All(Char.IsDigit) || (prov_do.All(Char.IsDigit)) && str_do[0] == '-')  ///ev
+                    {
+                        int int_ot = int.Parse(str_ot);
+                        int int_do = int.Parse(str_do);
+
+                        List<string> subs = data.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                                                                              .Select(s => s.Trim()).ToList();
+                        int dlina = subs.Count;
+                        if (int_ot > dlina)
+                        {
+                            vivod.Text = "";
+                            MessageBox.Show(
+                                "Первая граница больше, чем количество слов в строке"
+                                );
+                        }
+                        else
+                        if (int_ot <= 0) 
+                        {
+                            vivod.Text = "";
+                            MessageBox.Show(
+                                "Первая граница меньше 1"
+                                );
+                        }
+                        else
+                        if (int_do <= 0)
+                        {
+                            vivod.Text = "";
+                            MessageBox.Show(
+                                "Вторая граница меньше 1"
+                                );
+                        }
+                        else
+                        if (int_do > dlina)
+                        {
+                            vivod.Text = "";
+                            MessageBox.Show(
+                                "Вторая граница больше, чем количество слов в строке"
+                                );
+                        }
+                        else
+                        if (int_ot > int_do)
+                        {
+                            vivod.Text = "";
+                            MessageBox.Show(
+                                "Первая граница больше, чем вторая"
+                                );
+                        }
+                        else  //evv
+                        {
+                            int_ot--;
+                            int_do--;
+                            List<string> chast = new List<string>();
+                            for (int i = int_ot; i <= int_do; i++)
+                            {
+                                chast.Add(subs[i]);
+                            }
+                            subs.RemoveRange(int_ot, int_do - int_ot + 1);
+                            //int dlin_ost = subs.Count;
+                            String result = "";
+                            foreach (string s in chast)
+                            {
+                                result = result + " " + s;
+                            }
+                            foreach (string s in subs)
+                            {
+                                result = result + " " + s;
+                            }
+                            vivod.Text = result;
+                        }
+                        //vivod.Text = subs[0] + subs[2] + subs[1]; 
+                    }
+                }
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String data = vvod.Text;
+            //алгоритм с data
+            algor(data);
+        }
+    }
+}
